edcd33dbded3230a46928ab7ce42d54a95bf9028
ohne <ghdlr785@gmail.com>
2022-06-28 22:17:59 +0900

    초기 커밋.

---
+++ cote21/cote21.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) 
+{
+    bool answer = true;
+    int i;
+
+    if (s.size() != 4 && s.size() != 6) {
+        answer = false;
+        return answer;
+    }
+
+    for (i = 0; i < s.size(); i++) {
+        if (s[i] < 48 || s[i] > 57) {
+            answer = false;
+            break;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
